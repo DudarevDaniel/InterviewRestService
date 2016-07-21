562651279ccdd10538db4de0b14efeb4d56d8eb1
Daniel Dudarev <dudarev.daniel@gmail.com>
2016-07-21 22:01:00 +0300

    added persistence context test

--- src/test/java/com/example/helloworld/dao/ContactDaoTest.java
+++ src/test/java/com/example/helloworld/dao/ContactDaoTest.java
@@ -1,16 +1,26 @@
 package com.example.helloworld.dao;
 
 import com.example.helloworld.InterviewApplicationTests;
+import com.example.helloworld.dao.entity.Contact;
 import org.hibernate.ScrollableResults;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
 @Transactional
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, value = {"classpath:cleanUp.sql", "classpath:test-data.sql"})
 public class ContactDaoTest extends InterviewApplicationTests {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     private ContactDao contactDao;
@@ -26,6 +36,16 @@ public class ContactDaoTest extends InterviewApplicationTests {
             resultSize++;
         }
         Assert.assertEquals(5, resultSize);
+    }
+
+    @Test
+    public void testPersistenceContext() {
+        Session session = entityManager.unwrap(Session.class);
+        List<Contact> contactList = session.createCriteria(Contact.class)
+                .add(Restrictions.eq("name", "Aaron"))
+                .list();
+        Assert.assertTrue(contactList.size() == 1);
+        Assert.assertTrue(session.contains(contactList.get(0)));
     }
 
 }
